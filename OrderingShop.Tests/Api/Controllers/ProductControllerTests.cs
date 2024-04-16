624589206ae95f060973ee817dd26efdd46a6bcd
Janio Carvalho <janiocarvalhojr@gmail.com>
2024-04-16 20:28:17 -0300

    chore: create structure of test

---
+++ OrderingShop.Tests/Api/Controllers/ProductControllerTests.cs
@@ -0,0 +1,15 @@
+using OrderingShop.Api.Controllers.ProductController;
+
+namespace OrderingShop.Tests.Api.Controllers
+{
+    public class ProductControllerTests
+    {
+        private readonly ProductController _productController;
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
