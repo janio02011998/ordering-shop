aeb4cd36907699723b55d28de7a42d52bf0d000c
Janio Carvalho <janiocarvalhojr@gmail.com>
2024-04-17 20:24:55 -0300

    chore: create global imports

---
+++ OrderingShop.Tests/GlobalUsing.cs
@@ -0,0 +1,13 @@
+﻿global using System.Security.Claims;
+
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.AspNetCore.Mvc;
+
+global using OrderingShop.Api.Controllers.ProductController;
+global using OrderingShop.Domain.Interfaces;
+global using OrderingShop.Domain.Dtos;
+global using FluentAssertions;
+
+global using FakeItEasy;
+global using Xunit;
