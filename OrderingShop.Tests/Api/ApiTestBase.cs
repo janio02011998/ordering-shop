a1b12f86c61aef1fef991a1431e853455cfe68b1
Janio Carvalho <janiocarvalhojr@gmail.com>
2024-04-17 20:25:10 -0300

    chore: create ApiTestBase

---
+++ OrderingShop.Tests/Api/ApiTestBase.cs
@@ -0,0 +1,26 @@
+﻿
+namespace OrderingShop.Tests.Api
+{
+    public abstract class ApiTestBase
+    {
+        protected static readonly string validProductId = Guid.NewGuid().ToString();
+
+        protected HttpContextAccessor httpContextAccessor;
+
+        protected ApiTestBase()
+        {
+            var context = new HttpContextAccessor
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+
+            context.HttpContext.Request.Headers["Authorization"] = "Baerer some_fake";
+         
+            var fakeUser = A.Fake<ClaimsPrincipal>();
+            A.CallTo(() => fakeUser.Identity.IsAuthenticated).Returns(true);
+            context.HttpContext.User = fakeUser;
+
+            httpContextAccessor = context;
+        }
+    }
+}
